430ed17c82f9828bbeefe9c9e225d428354fb37b
mateus <mateusinaciorocha005@gmail.com>
2025-04-22 00:48:26 -0300

    usando var ambinete

--- backend/api/connect.js
+++ backend/api/connect.js
@@ -1,8 +1,14 @@
-// "mongodb+srv://mateusinaciorocha005:naruto123741@cluster0.vt9qq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-import { MongoClient } from 'mongodb';
-const URI = 'mongodb+srv://mateusinaciorocha005:naruto123741@cluster0.vt9qq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'; //string de conexão com o banco de dados
-const client = new MongoClient(URI); //conecta com o cluster de dados
 
-export const db = client.db('Spotify'); //pega o banco de dados dentro do cluster
+import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const client = new MongoClient(process.env.connectURI); //conecta com o cluster de dados
+
+export const db = client.db(process.env.DBname); //pega o banco de dados dentro do cluster
 // const songCollection = await db.collection('songs').find({}).toArray(); //pega a collection (tabela) dentro do banco de dados
 // console.log(songCollection);
+
+
+
+
